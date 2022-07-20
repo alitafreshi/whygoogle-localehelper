c77e4804273656fe2537dcbcb9c568fedc49ec29
ali tafreshi <alitafreshi123@yahoo.com>
2022-07-20 16:07:09 +0430

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,22 @@
 plugins {
-    id(Plugins.androidLibrary)
+    id(Plugins.androidApplication)
     kotlin(KotlinPlugins.android)
-    id(KotlinPlugins.maven)
 }
 
 android {
     compileSdk = Application.compileSdk
 
     defaultConfig {
+        applicationId = Application.appId
         minSdk = Application.minSdk
         targetSdk = Application.targetSdk
+        versionCode = Application.versionCode
+        versionName = Application.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -25,15 +29,11 @@ android {
         }
     }
 
-    buildFeatures {
-        viewBinding = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
 
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -49,29 +49,4 @@ dependencies {
     androidTestImplementation(Junit.junitTestExt)
     androidTestImplementation(Junit.junitTestExtKtx)
     androidTestImplementation(Espresso.espresso)
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            register("release", MavenPublication::class) {
-                // Applies the component for the release build variant.
-                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
-                from(components["release"])
-
-
-                // Library Package Name (Example : "com.frogobox.androidfirstlib")
-                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
-                groupId = "com.github.alitafreshi"
-
-                // Library Name / Module Name (Example : "androidfirstlib")
-                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
-                artifactId = "whygoogle-localehelper"
-
-                // Version Library Name (Example : "1.0.0")
-                version = "0.0.1"
-            }
-        }
-    }
 }
--- locale-helper/build.gradle.kts
+++ locale-helper/build.gradle.kts
@@ -1,21 +1,22 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.androidLibrary)
+    kotlin(KotlinPlugins.android)
+    id(KotlinPlugins.maven)
 }
 
 android {
-    compileSdk = 32
+    compileSdk = Application.compileSdk
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 32
+        minSdk = Application.minSdk
+        targetSdk = Application.targetSdk
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -23,10 +24,16 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -34,10 +41,36 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(AndroidX.coreKtx)
+    implementation(AndroidX.appCompatActivity)
+    implementation(MaterialDesign.materialDesign)
+    implementation(AndroidX.constraintLayout)
+    testImplementation(Junit.junit)
+    androidTestImplementation(Junit.junitTestExt)
+    androidTestImplementation(Junit.junitTestExtKtx)
+    androidTestImplementation(Espresso.espresso)
+}
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+
+
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = "com.github.alitafreshi"
+
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = "whygoogle-localehelper"
+
+                // Version Library Name (Example : "1.0.0")
+                version = "0.0.1"
+            }
+        }
+    }
 }
